212690bb168c4fe573db19030bc40bf6b6ed2ed1
BuildTools <unconfigured@null.spigotmc.org>
2021-01-29 06:31:39 +0800

    updated server and created controller ui

--- rasp_src/flask_test_dev.py
+++ rasp_src/flask_test_dev.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env.python3
 import serial
-import time
 from flask import Flask, request
+import threading
 
 app = Flask(__name__)
+
+def serial_logger():
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
 
 @app.route('/open', methods=['GET'])
 def open():
@@ -23,7 +29,16 @@ def stop():
     line = ser.readline().decode('utf-8').rstrip()
     return line
 
+@app.route('/forward', methods=['GET'])
+def forward():
+    ser.write(b"4")
+    line = ser.readline().decode('utf-8').rstrip()
+    return line
+
 if __name__ == "__main__":
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyACM1', 115200, timeout=1)
     ser.flush()
-    app.run(threaded=True)
+    flask_thread = threading.Thread(target=app.run(threaded=True))
+    flask_thread.start()
+    serial_logger()
+    
